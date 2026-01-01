65a1e16f189a9dc1f79913be55f71377475ae083
HariKrishnan <krish96882@gmail.com>
2026-01-01 20:47:40 +0530

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,7 @@
+function showSection(sectionId) {
+  document.querySelectorAll(".content-section").forEach(section => {
+    section.classList.remove("active");
+  });
+
+  document.getElementById(sectionId).classList.add("active");
+}
